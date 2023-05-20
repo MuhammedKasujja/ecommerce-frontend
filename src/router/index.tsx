58f5ade978893393780d85c5948d82ea75b1edd6
Muhammed Kasujja <al.kasmud.2@gmail.com>
2023-05-20 10:44:57 +0300

    feat: add fallback route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "src/App";
 import {
   AddBrand,
@@ -119,6 +119,10 @@ const router = createBrowserRouter([
       // end Products
     ],
   },
+  {
+    path:"*",
+    element:<Navigate to="/" replace />
+  }
 ]);
 
 export default router;
